9d9834dab8798d0f0925c3532a135a0ae1e1775b
Ola <hollabodey.kilaso@gmail.com>
2023-05-19 10:21:42 +0100

    Made changes to the html and css files

---
+++ script.js
@@ -0,0 +1,10 @@
+let numbers = document.querySelectorAll('.number1');
+let nextBtn = document.querySelector('.nextBtn');
+
+
+let step1 = document.querySelector('.Step1');
+
+nextBtn.addEventListener('click', () => {
+    // alert('Next button clicked!');
+    step1.style.display = 'none';
+});
