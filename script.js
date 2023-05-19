107a5afc154b6230953b6e939c0d3a1fee6fb379
Ola <hollabodey.kilaso@gmail.com>
2023-05-19 10:30:54 +0100

    Made changes to the html and css files

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let nextBtn = document.querySelector('.nextBtn');
 let step1 = document.querySelector('.Step1');
 
 nextBtn.addEventListener('click', () => {
-    // alert('Next button clicked!');
-    step1.style.display = 'none';
+    // step1.style.display = 'none';
+    numbers.classList.add('active');
 });
+
+
+
+// forEach( (item) => {item.classList.add('active');}
