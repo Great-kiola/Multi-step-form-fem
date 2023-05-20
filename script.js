e7531f35e59b61610a3b123eee67907ce43b3690
Ola <hollabodey.kilaso@gmail.com>
2023-05-20 05:36:29 +0100

    Made changes to the html and css file

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let numbers = document.querySelectorAll('.number1');
 let nextBtn = document.querySelector('.nextBtn');
+let toggle = document.querySelector('.toggle');
 
 
 let step1 = document.querySelector('.Step1');
 
 nextBtn.addEventListener('click', () => {
     step1.style.display = 'none';
-    // numbers.classList.add('active');
 });
 
+
+toggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggle.classList.toggle('after');
+});
 
 
 // forEach( (item) => {item.classList.add('active');}
