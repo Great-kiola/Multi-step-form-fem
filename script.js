04909a733ced18785a3806b3e3b9849ae65073d4
Ola <hollabodey.kilaso@gmail.com>
2023-05-21 21:06:58 +0100

    Made changes to the css and js file

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let numbers = document.querySelectorAll('.number1');
 let nextBtn = document.querySelector('.nextBtn');
 let toggle = document.querySelector('.toggle');
+let prevBtn = document.querySelector('.prevBtn');
 
 let yearly = document.querySelector('.yearly');
 let monthly = document.querySelector('.monthly');
 
 let step1 = document.querySelector('.Step1');
+let step2 = document.querySelector('.step2');
 
 nextBtn.addEventListener('click', () => {
     step1.style.display = 'none';
+    step2.style.display = 'block';
+});
+
+prevBtn.addEventListener('click', () => {
+    step1.style.display = 'block';
+    step2.style.display = 'none';
 });
 
 
