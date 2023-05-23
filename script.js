ae4d23775b53592a2f40301feeca5c73b6b958fd
Ola <hollabodey.kilaso@gmail.com>
2023-05-23 20:39:13 +0100

    Made changes to the html and css file

--- script.js
+++ script.js
@@ -11,9 +11,26 @@ let step2 = document.querySelector('.step2');
 
 let planBox = document.querySelector('.planBox');
 
+let errorDiv = document.querySelector('.errorMsg2');
+let eAddressInput = document.querySelector('#email');
+
 nextBtn.addEventListener('click', () => {
-    step1.style.display = 'none';
-    step2.style.display = 'block';
+
+    function submit(){
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    
+        if(eAddressInput.value.match(validRegex)){
+            errorDiv.style.display = 'none';
+            step1.style.display = 'none';
+            step2.style.display = 'block';
+            return true;
+        }else {
+            errorDiv.style.display = 'block';
+        }
+    }
+
+    submit();
+
 });
 
 prevBtn.addEventListener('click', () => {
@@ -31,5 +48,9 @@ planBox.addEventListener('click', () => {
     alert('clicked!')
     planBox.classList.add('active');
 });
+
+
+
+
 
 // forEach( (item) => {item.classList.add('active');}
