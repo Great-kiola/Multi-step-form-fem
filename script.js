d75a2f1c088e8ba866a4c2789e50195396e79ab5
Ola <hollabodey.kilaso@gmail.com>
2023-05-23 20:15:58 +0100

    Made changes to the html, css and js file

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let monthly = document.querySelector('.monthly');
 let step1 = document.querySelector('.Step1');
 let step2 = document.querySelector('.step2');
 
+let planBox = document.querySelector('.planBox');
+
 nextBtn.addEventListener('click', () => {
     step1.style.display = 'none';
     step2.style.display = 'block';
@@ -25,5 +27,9 @@ toggle.addEventListener('click', (e) => {
     toggle.classList.toggle('after');
 });
 
+planBox.addEventListener('click', () => {
+    alert('clicked!')
+    planBox.classList.add('active');
+});
 
 // forEach( (item) => {item.classList.add('active');}
