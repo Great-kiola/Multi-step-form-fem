a211ec20f240aea559016c0761da0da2bec15fd4
Ola <hollabodey.kilaso@gmail.com>
2023-05-27 09:37:37 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numbers = document.querySelectorAll(".number1");
+let numbers = document.querySelector(".numbers");
 let nextBtn = document.querySelector(".nextBtn");
 let toggle = document.querySelector(".toggle");
 let prevBtn = document.querySelector(".prevBtn");
@@ -31,39 +31,76 @@ let prevBtn4 = document.getElementById("prevBtn4");
 let nextBtn4 = document.getElementById("nextBtn4");
 let PrevBtn4 = document.getElementById("prevBtn4");
 
-nextBtn.addEventListener("click", () => {
-    function submit() {
+// Handles what happens when you click the first submit button
+// nextBtn.addEventListener("click", () => {
+//     function submit() {
 
-        // Check if the name is valid.
-        if (!nameInput.value){
-            errorDiv1.style.display = "block";
-        } else {
-            errorDiv1.style.display = "none";
-        }
+//         // Check if the name is valid.
+//         if (!nameInput.value){
+//             errorDiv1.style.display = "block";
+//         } else {
+//             errorDiv1.style.display = "none";
+//         }
 
-        // Form validation for email addresses
-        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        
-        if (eAddressInput.value.match(validRegex)) {
-            errorDiv2.style.display = "none";
-        } else {
-            errorDiv2.style.display = "block";
-        }
+//         // Form validation for email addresses
+//         let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-        // Checks if phone number is valid
-        if (!phoneInput.value){
-            errorDiv3.style.display = "block";
-        } else {
-            errorDiv3.style.display = "none";
-        }
+//         if (eAddressInput.value.match(validRegex)) {
+//             errorDiv2.style.display = "none";
+//         } else {
+//             errorDiv2.style.display = "block";
+//         }
 
+//         // Checks if phone number is valid
+//         if (!phoneInput.value){
+//             errorDiv3.style.display = "block";
+//         } else {
+//             errorDiv3.style.display = "none";
+//         }
+
+//     }
+
+//     step1.style.display = "none";
+//     step2.style.display = "block";
+
+//     submit();
+// });
+
+nextBtn.addEventListener("click", handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    // Check if the name is valid.
+    if (!nameInput.value) {
+        errorDiv1.style.display = "block";
+    } else {
+        errorDiv1.style.display = "none";
     }
 
-    submit();
+    // Form validation for email addresses
+    let validRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (eAddressInput.value.match(validRegex)) {
+        errorDiv2.style.display = "none";
+    } else {
+        errorDiv2.style.display = "block";
+    }
+
+    // Checks if phone number is valid
+    if (!phoneInput.value) {
+        errorDiv3.style.display = "block";
+    } else {
+        errorDiv3.style.display = "none";
+    }
+
+    numbers.forEach(num => num.classList.remove("active"));
 
     // step1.style.display = "none";
     // step2.style.display = "block";
-});
+
+}
 
 prevBtn.addEventListener("click", () => {
     step1.style.display = "flex";
@@ -104,7 +141,6 @@ nextBtn4.addEventListener("click", () => {
     step5.style.display = "block";
     step4.style.display = "none";
 });
-
 
 // if (!nameInput.value){
 //     errorDiv1.style.display = 'none';
