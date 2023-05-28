066d19b1d5312588b5cfd4892eb6b370c3462aa4
Ola <hollabodey.kilaso@gmail.com>
2023-05-28 15:26:20 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -31,40 +31,7 @@ let prevBtn4 = document.getElementById("prevBtn4");
 let nextBtn4 = document.getElementById("nextBtn4");
 let PrevBtn4 = document.getElementById("prevBtn4");
 
-// Handles what happens when you click the first submit button
-// nextBtn.addEventListener("click", () => {
-//     function submit() {
 
-//         // Check if the name is valid.
-//         if (!nameInput.value){
-//             errorDiv1.style.display = "block";
-//         } else {
-//             errorDiv1.style.display = "none";
-//         }
-
-//         // Form validation for email addresses
-//         let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-//         if (eAddressInput.value.match(validRegex)) {
-//             errorDiv2.style.display = "none";
-//         } else {
-//             errorDiv2.style.display = "block";
-//         }
-
-//         // Checks if phone number is valid
-//         if (!phoneInput.value){
-//             errorDiv3.style.display = "block";
-//         } else {
-//             errorDiv3.style.display = "none";
-//         }
-
-//     }
-
-//     step1.style.display = "none";
-//     step2.style.display = "block";
-
-//     submit();
-// });
 
 nextBtn.addEventListener("click", handleSubmit);
 
@@ -153,3 +120,39 @@ nextBtn4.addEventListener("click", () => {
 // errorDiv2.style.display = 'none';
 
 // forEach( (item) => {item.classList.add('active');}
+
+
+// Handles what happens when you click the first submit button
+// nextBtn.addEventListener("click", () => {
+//     function submit() {
+
+//         // Check if the name is valid.
+//         if (!nameInput.value){
+//             errorDiv1.style.display = "block";
+//         } else {
+//             errorDiv1.style.display = "none";
+//         }
+
+//         // Form validation for email addresses
+//         let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+//         if (eAddressInput.value.match(validRegex)) {
+//             errorDiv2.style.display = "none";
+//         } else {
+//             errorDiv2.style.display = "block";
+//         }
+
+//         // Checks if phone number is valid
+//         if (!phoneInput.value){
+//             errorDiv3.style.display = "block";
+//         } else {
+//             errorDiv3.style.display = "none";
+//         }
+
+//     }
+
+//     step1.style.display = "none";
+//     step2.style.display = "block";
+
+//     submit();
+// });
